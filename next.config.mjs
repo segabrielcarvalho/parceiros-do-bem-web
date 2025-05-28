cf203a551db7ee135ed42a94133c9cf3acc0b942
Gabriel Carvalho <gabriel.carvalho@grupoigd.com.br>
2025-05-28 15:30:16 -0300

    Chore - Remove rotas e layouts de app e ajusta config do Next
    Remove páginas e layouts customizados da aplicação e autenticação, além de rotas protegidas, simplificando a estrutura do projeto.
    Adiciona configurações no Next para uso de imagens remotas, otimização com 'sharp' e arquivos de cache do pnpm.
    Inclui arquivo robots.txt e personaliza spell-check no VSCode.
    
    Reflete uma reestruturação geral inicial.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 export default nextConfig
